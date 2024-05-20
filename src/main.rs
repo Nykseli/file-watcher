d3bb5720b216ea5358ee78a70711a4955ef7abd9
Miika Alikirri <nykseli@protonmail.com>
2024-05-20 22:41:58 +0300

    Basic watching of current directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use nix::{
+    libc::AT_FDCWD,
+    sys::fanotify::{
+        EventFFlags, Fanotify, FanotifyResponse, InitFlags, MarkFlags, MaskFlags, Response,
+    },
+};
+
 fn main() {
-    println!("Hello, world!");
+    let notify = Fanotify::init(
+        InitFlags::FAN_CLOEXEC | InitFlags::FAN_CLASS_CONTENT, // | InitFlags::FAN_NONBLOCK,
+        EventFFlags::O_RDONLY | EventFFlags::O_LARGEFILE,
+    )
+    .unwrap();
+
+    notify
+        .mark(
+            MarkFlags::FAN_MARK_ADD | MarkFlags::FAN_MARK_MOUNT,
+            MaskFlags::FAN_OPEN_PERM | MaskFlags::FAN_CLOSE_WRITE,
+            Some(AT_FDCWD),
+            Some("."),
+        )
+        .unwrap();
+
+    loop {
+        for event in notify.read_events().unwrap().iter() {
+            if event.mask().contains(MaskFlags::FAN_OPEN_PERM) {
+                notify
+                    .write_response(FanotifyResponse::new(
+                        event.fd().unwrap(),
+                        Response::FAN_ALLOW,
+                    ))
+                    .unwrap();
+            }
+            println!("{event:#?}");
+        }
+    }
 }
